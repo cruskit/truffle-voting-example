d1453a80f780c216053134b245c90c984e891960
Paul Ruskin <cruskit@gmail.com>
2017-08-20 16:37:34 +1000

    Initial commit. Tests not passing and still need a lot of work.

---
+++ test/ballot.js
@@ -0,0 +1,57 @@
+var Ballot = artifacts.require("./Ballot.sol");
+
+contract('Ballot', function(accounts) {
+    it("should return 0 as the ballot winner", function() {
+        return Ballot.deployed().then(function(instance) {
+            return instance.winningProposal.call();
+        }).then(function(winner) {
+            assert.equal(winner.valueOf(), 0, "0 was the winner");
+        });
+    });
+    it("should be able to add a voter and vote", function() {
+
+        var ballot;
+        var proposals = ["first", "second"];
+        var account_zero = "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1";
+        var account_two = "0x22d491bde2303f2f43325b2108d26f1eaba1e32b";
+
+        return Ballot.new(proposals).then(function(instance) {
+            ballot = instance;
+            console.log("ballot: " + ballot);
+            for(var propName in ballot) {
+                propValue = ballot[propName]
+
+                console.log(propName,propValue);
+            }
+            return ballot.giveRightToVote.call(account_two, {from: account_zero} );
+        }).then(function() {
+            return ballot.vote.call(1, {from: account_two});
+        }).then(function() {
+            return ballot.winningProposal.call();
+        }).then(function(winner) {
+            assert.equal(winner.valueOf(), 1, "1 was the winner");
+        });
+    });
+
+    it("should not allow voting twice", function() {
+
+        var ballot;
+        var proposals = ["first", "second"];
+        var account_zero = "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1";
+        var account_two = "0x22d491bde2303f2f43325b2108d26f1eaba1e32b";
+
+        return Ballot.new(proposals).then(function(instance) {
+            ballot = instance;
+            return ballot.giveRightToVote.call(account_two, {from: account_zero} );
+        }).then(function() {
+            return ballot.vote.call(1, {from: account_two});
+//        }).then(function() {
+  //          return ballot.vote.call(2, {from: account_two});
+        }).then(function() {
+            return ballot.winningProposal.call();
+        }).then(function(winner) {
+            assert.equal(winner.valueOf(), 1, "1 was the winner");
+        });
+    });
+
+});
